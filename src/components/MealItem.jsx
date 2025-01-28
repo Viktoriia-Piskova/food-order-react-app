a681abeec89cb82c2ef7ab2c2520d3ade4a1ad3d
piskova <v.piskova@beetroot.se>
2025-01-28 16:14:15 +0200

    Add currency formatting

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { currencyFormatter } from "../utils/formatting";
 
-const backendUrl = 'http://localhost:3000/'
+const backendUrl = "http://localhost:3000/";
 
 const MealItem = ({ meal }) => {
   return (
@@ -9,7 +10,9 @@ const MealItem = ({ meal }) => {
         <img src={`${backendUrl}${meal.image}`} alt={meal.name} />
         <div>
           <h3>{meal.name}</h3>
-          <p className="meal-item-price">{meal.price}</p>
+          <p className="meal-item-price">
+            {currencyFormatter.format(meal.price)}
+          </p>
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
