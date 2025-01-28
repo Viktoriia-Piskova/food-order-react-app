f0eb1ff04d22a825635ee9f9ac8f1a67987255fd
piskova <v.piskova@beetroot.se>
2025-01-28 16:20:52 +0200

    Add Button UI component

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { currencyFormatter } from "../utils/formatting";
+import Button from './UI/Button'
+
 
 const backendUrl = "http://localhost:3000/";
 
@@ -16,7 +18,7 @@ const MealItem = ({ meal }) => {
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <button>Add to Cart</button>
+          <Button>Add to Cart</Button>
         </p>
       </article>
     </li>
